63faddcb2e4a87186dee69131381ee1f48296c61
gauravbarakoti <bestgaurav1234@gmail.com>
2023-10-30 18:56:01 +0530

    some changes in code

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
 
@@ -11,32 +8,38 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        // You don't have any code for the Page_Load event, so you can remove it.
     }
+
     protected void Button1_Click(object sender, EventArgs e)
     {
-        SqlConnection con = new SqlConnection(@"Data Source=dotnetdb1.cvyxdllt3sz6.us-east-1.rds.amazonaws.com;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True;User Instance=True");
-        SqlCommand cmd = new SqlCommand("select  * from data", con);
-        cmd.Parameters.AddWithValue("@username", TextBox1.Text);
-        cmd.Parameters.AddWithValue("password", TextBox2.Text);
-        SqlDataAdapter sda = new SqlDataAdapter(cmd);
-        DataTable dt = new DataTable();
-        sda.Fill(dt);
-        con.Open();
-        int i = cmd.ExecuteNonQuery();
-        con.Close();
+        string connectionString = "Data Source=dotnetdb1.cvyxdllt3sz6.us-east-1.rds.amazonaws.com;";
 
-        if (dt.Rows.Count > 0)
+        using (SqlConnection con = new SqlConnection(connectionString))
         {
-            Response.Redirect("Redirectform.aspx");
-        }
-        else
-        {
-            Label1.Text = "You're username and password is incorrect";
-            Label1.ForeColor = System.Drawing.Color.Red;
-        
-        }
+            con.Open();
+            string query = "SELECT * FROM info";
+            using (SqlCommand cmd = new SqlCommand(query, con))
+            {
+                cmd.Parameters.AddWithValue("@username", TextBox1.Text);
+                cmd.Parameters.AddWithValue("@password", TextBox2.Text);
 
-        
+                using (SqlDataAdapter sda = new SqlDataAdapter(cmd))
+                {
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        Response.Redirect("Redirectform.aspx");
+                    }
+                    else
+                    {
+                        Label1.Text = "Your username and password are incorrect";
+                        Label1.ForeColor = System.Drawing.Color.Red;
+                    }
+                }
+            }
+        }
     }
 }
